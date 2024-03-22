1bdd34659f49b0ebd5a0f5c05b8f108330b6da62
Vee <ifebuchejulia@gmail.com>
2024-03-22 21:39:32 +0100

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+         <h1>
+        About <sup>(03)</sup>
+      </h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+<h1>
+        Contact <sup>(04)</sup>
+      </h1>
+    </div>
+  )
+}
+
+export default page
