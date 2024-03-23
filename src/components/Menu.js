eacb6a470d24430b76ee0a33c1fa9208ecb4dafb
Vee <ifebuchejulia@gmail.com>
2024-03-23 08:08:57 +0100

    content update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,34 +1,63 @@
 "use client";
 
-import React, {useEffect, useRef, useState} from 'react';
-import Link from 'next/link';
-import "./menu/menu.css"
+import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import "./menu/menu.css";
 
 const menuLinks = [
-    {path: "/", label: "Home"},
-    {path: "/work", label: "Work"},
-    {path: "/about", label: "About"},
-    {path: "/contact", label: "Contact"},
-    {path: "/lab", label: "Lab"},
+  { path: "/", label: "Home" },
+  { path: "/work", label: "Work" },
+  { path: "/about", label: "About" },
+  { path: "/contact", label: "Contact" },
+  { path: "/lab", label: "Lab" },
 ];
 const Menu = () => {
-const container = useRef();
-const [isMenuOpen, setIsMenuOpen] = useState(false);
-const toggleMenu = ()=>{
-    setIsMenuOpen(!isMenuOpen)
-}
+  const container = useRef();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <div className='menu-container' ref={container}>
-    <div className='menu-bar'>
-        <div className='menu-logo'>
-            <Link href="/">Vera</Link>
+    <div className="menu-container" ref={container}>
+      <div className="menu-bar">
+        <div className="menu-logo">
+          <Link href="/">Vera</Link>
         </div>
-        <div className='menu-open'>Menu</div>
+        <div className="menu-open">
+          <p className="">Menu</p>
+        </div>
+      </div>
+      <div className="menu-overlay">
+        <div className="menu-overlay-bar">
+          <div className="menu-logo">
+            <Link href="/">Vera</Link>
+          </div>
+          <div className="meu-close">
+            <p>close</p>
+          </div>
+        </div>
+        <div className="menu-close-icon">
+          <p>&#x2715;</p>
+        </div>
+        <div className="menu-copy">
+          <div className="menu-links">
+            {menuLinks.map((link, index)=>{
+              <div className="menu-link-item" key={index}>
+                <div className="menu-link-item-holder">
+                  <Link href={link.path} className="menu-link">
+                    {link.label}
+                  </Link>
+                </div>
+              </div>
+            })}
+          </div>
+          <div className="menu-info"></div>
+        </div>
+        <div className="menu-preview"></div>
+      </div>
     </div>
-    <div className='menu-overlay'></div>
-    </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
