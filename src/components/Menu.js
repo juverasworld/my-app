35407932d402dae1bf2720c68c6c3d3bca966a8f
Vee <ifebuchejulia@gmail.com>
2024-03-26 10:03:24 +0100

    content update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,80 +1,130 @@
 "use client"
 
-import React, {  useRef, useState } from "react";
-import Link from "next/link";
-import "./menu/menu.css";
+import { useEffect, useRef } from 'react';
+import { gsap, Power2 } from 'gsap';
 
-const menuLinks = [
-  { path: "/", label: "Home" },
-  { path: "/work", label: "Work" },
-  { path: "/about", label: "About" },
-  { path: "/contact", label: "Contact" },
-  { path: "/lab", label: "Lab" },
-];
+import "./menu/menu.css"
+const YourComponent = () => {
+  const menuToggleRef = useRef(null);
+  const menuBar = useRef(gsap.timeline());
+  const tl = useRef(gsap.timeline({ paused: true }));
 
-const Menu = () => {
-  const container = useRef();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  useEffect(() => {
+    const menuToggle = menuToggleRef.current;
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-  
+    menuBar.current.to('.bar-1', 0.5, {
+      attr: { d: "M8,2 L2,8" },
+      x: 1,
+      ease: Power2.easeInOut
+    }, 'start');
+
+    menuBar.current.to('.bar-2', 0.5, {
+      autoAlpha: 0
+    }, 'start');
+
+    menuBar.current.to('.bar-3', 0.5, {
+      attr: { d: "M8,8 L2,2" },
+      x: 1,
+      ease: Power2.easeInOut
+    }, 'start');
+
+    tl.current.to('.fullpage-menu', {
+      duration: 0,
+      display: "block",
+      ease: 'Expo.easeInOut',
+    });
+
+    tl.current.from('.menu-bg span', {
+      duration: 1,
+      x: "100%",
+      stagger: 0.1,
+      ease: 'Expo.easeInOut'
+    });
+
+    tl.current.from('.main-menu li a', {
+      duration: 1.5,
+      y: "100%",
+      stagger: 0.2,
+      ease: 'Expo.easeInOut'
+    }, "-=0.5");
+
+    tl.current.from('.social-links li', {
+      duration: 1,
+      y: "-100%",
+      opacity: 0,
+      stagger: 0.1,
+      ease: 'Expo.easeInOut'
+    }, "-=0.5");
+
+    tl.current.reverse();
+
+    menuToggle.addEventListener('click', function () {
+      menuBar.current.reversed(!menuBar.current.reversed());
+      tl.current.reversed(!tl.current.reversed());
+    });
+
+    // Cleanup
+    return () => {
+      menuToggle.removeEventListener('click');
+    };
+  }, []);
+
   return (
-    <div className="menu-container" ref={container}>
-      <div className="menu-bar">
-        <div className="menu-logo">
-          <Link href="/">Vera</Link>
-        </div>
-        <div className="menu-open" onClick={toggleMenu}>
-          <p>Menu</p>
-        </div>
-      </div>
+    <div>
+      <header>
+        <div className="header-row">
+            <div className="brand-logo">
+                <a href="#">
+                Vera
+                </a>
+            </div>
+            <button className="menu-toggle" id="menuToggle" ref={menuToggleRef}>
+                <svg viewBox="0 0 12 10" className="hamburger" height="40px" width="40px">
+                    <path d="M10,2 L2,2" className="bar-1"></path>
+                    <path d="M2,5 L10,5" className="bar-2"></path>
+                    <path d="M10,8 L2,8" className="bar-3"></path>
 
-      <div className="menu-overlay">
-        <div className="menu-overlay-bar">
-          <div className="menu-logo">
-            <Link href="/">Vera</Link>
-          </div>
-          <div className="menu-close" onClick={toggleMenu}>
-            <p>Close</p>
-          </div>
+                </svg>
+            </button>
         </div>
-        <div className="menu-close-icon">
-          <p>&#x2715;</p>
-        </div>
-        <div className="menu-copy">
-          <div className="menu-links">
-            {menuLinks.map((link, index) => (
-              <div className="menu-link-item" key={index}>
-                <div className="menu-link-item-holder" onClick={toggleMenu}>
-                  <Link href={link.path} className="menu-link">
-                    {link.label}
-                  </Link>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="menu-info">
-            <div className="menu-info-col">
-              <a href="#">X &#8599;</a>
-              <a href="#">Instagram &#8599;</a>
-              <a href="#">LinkedIn &#8599;</a>
-              <a href="#">Behance &#8599;</a>
-              <a href="#">Dribble &#8599;</a>
+    </header>
+
+    <section className="fullpage-menu">
+        <div className="fullpage-menu-inner">
+            <div className="menu-bg">
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
-            <div className="menu-info-col">
-              <p>info@vera.gmail.com</p>
-              <p>0915732332222</p>
+
+            <nav>
+                <ul className="main-menu">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Work</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+
+            <div className="header-nav-footer">
+                <ul className="social-links">
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li>&copy;2021</li>
+                </ul>
             </div>
-          </div>
+
         </div>
-        <div className="menu-preview">
-          <p>VIEW SHOWREEL</p>
-        </div>
-      </div>
+    </section>
+
+    <section className="placeholder-title">
+        <h1>Fullpage Navigation Menu with <span>GSAP</span></h1>
+    </section>
+      {/* <button >Toggle Menu</button> */}
+      {/* Other JSX elements */}
     </div>
   );
 };
 
-export default Menu;
+export default YourComponent;
